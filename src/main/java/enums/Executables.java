b136136a4883b67d820eb0ac5dff990f44526f8b
rodolfo.mindtek <rodolfo.mindtek@CHI00557158.ibge.gov.br>
2019-01-18 15:55:11 -0200

    criando padão factory no enum

--- src/main/java/enums/Executables.java
+++ src/main/java/enums/Executables.java
@@ -1,11 +1,26 @@
 package enums;
 
+import br.com.nbis.Commands;
+import br.com.nbis.wsq.CommandsCWSQ;
+import br.com.nbis.wsq.CommandsMINDTCT;
+import br.com.nbis.wsq.CommandsNFIQ;
+
 public enum Executables {
 
-	BOZORTH3,
-	CWSQ,
-	DWSQ,
-	MINDTCT,
-	NFIQ;
+	BOZORTH3(null),
+	CWSQ(new CommandsCWSQ()),
+	DWSQ(null),
+	MINDTCT(new CommandsMINDTCT()),
+	NFIQ(new CommandsNFIQ());
+	
+	private Commands commands;
+	
+	private Executables(Commands commands) {
+		this.commands = commands;
+	}
+	
+	public Commands getCommands() {
+		return commands;
+	}
 	
 }
