ef940a0dc4a097b50e7994f08a8b77f9679383d8
rodolfo.mindtek <rodolfo.mindtek@CHI00557158.ibge.gov.br>
2019-01-18 15:54:36 -0200

    desenvolvendo classes de comando

---
+++ src/main/java/br/com/nbis/wsq/CommandsMINDTCT.java
@@ -0,0 +1,28 @@
+package br.com.nbis.wsq;
+
+import java.io.File;
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import br.com.nbis.Commands;
+
+public class CommandsMINDTCT implements Commands{
+	
+	private final Logger log = LogManager.getLogger(getClass());
+
+	@Override
+	public String[] commands(File fileExec, File filePath) {
+		String[] commands = null;
+		
+		try {
+			commands = Stream.of(fileExec.getAbsolutePath(), "-b", "-m1", filePath.getAbsolutePath(), filePath.getName()).toArray(String[]::new);
+		} catch (Exception e) {
+			log.error("Erro ao criar comando mindtct", e);
+		}
+		
+		return commands;
+	}
+
+}
