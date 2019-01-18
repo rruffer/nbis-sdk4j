12e1fa7ecb72e141312476c6e9c80c5f5035c3f0
rodolfo.mindtek <rodolfo.mindtek@CHI00557158.ibge.gov.br>
2019-01-18 15:55:38 -0200

    criando teste

--- src/test/java/br/com/nbis/exec/ExecTest.java
+++ src/test/java/br/com/nbis/exec/ExecTest.java
@@ -17,12 +17,46 @@ class ExecTest {
 		
 		Exec exec = new Exec();
 		
-		File file = UtilLoader.getFile("/img/anelar-dir.png");
+//		File file = UtilLoader.getFile("/img/anelar-dir.png");
+		File file = new File("anelar-dir.png");
 		
 		File execute = exec.execute(file, Executables.CWSQ);
 		
 		assertTrue(execute.exists());
 		
+		
+	}
+	
+	@Test
+	void executeNFIQ() {
+
+		Exec exec = new Exec();
+
+//		File file = UtilLoader.getFile("/img/anelar-dir.png");
+		File file = new File("anelar-dir.png");
+		File fileWSQ = new File("anelar-dir.wsq");
+		
+		exec.execute(file, Executables.CWSQ);
+		exec.execute(fileWSQ, Executables.NFIQ);
+
+//		assertTrue(execute.exists());
+
+	}
+	
+	@Test
+	void executeMINDTCT() {
+		
+		Exec exec = new Exec();
+		
+//		File file = UtilLoader.getFile("/img/anelar-dir.png");
+		File file = new File("anelar-dir.png");
+		File fileWSQ = new File("anelar-dir.wsq");
+		
+		exec.execute(file, Executables.CWSQ);
+		exec.execute(fileWSQ, Executables.NFIQ);
+		exec.execute(fileWSQ, Executables.MINDTCT);
+		
+//		assertTrue(execute.exists());
 		
 	}
 
