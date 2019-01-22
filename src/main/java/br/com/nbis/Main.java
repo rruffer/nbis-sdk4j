3697d1bf7c7472b017d465cf1077a33f2606ead7
rodolfo.mindtek <rodolfo.mindtek@CHI00557158.ibge.gov.br>
2019-01-22 16:08:11 -0200

    teste apache tika

--- src/main/java/br/com/nbis/Main.java
+++ src/main/java/br/com/nbis/Main.java
@@ -3,11 +3,8 @@ package br.com.nbis;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.tika.Tika;
 
 /**
  * {@link} https://www.geeksforgeeks.org/jvm-shutdown-hook-java/
@@ -18,34 +15,42 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		System.out.println(System.getProperty("java.io.tmpdir"));
-
-		File srcFile = new File("anelar-dir.png");
-		File baseDir = new File(System.getProperty("java.io.tmpdir") + "nbis");
-
-		baseDir.mkdir();
-
-		System.out.println(baseDir.getAbsolutePath());
-
-		// forma 1
-		FileUtils.copyFileToDirectory(srcFile, baseDir);
+//		System.out.println(System.getProperty("java.io.tmpdir"));
+//
+//		File srcFile = new File("anelar-dir.png");
+//		File baseDir = new File(System.getProperty("java.io.tmpdir") + "nbis");
+//
+//		baseDir.mkdir();
+//
+//		System.out.println(baseDir.getAbsolutePath());
+//
+//		// forma 1
+//		FileUtils.copyFileToDirectory(srcFile, baseDir);
 
 		// forma 2
 /*		Path copied = Paths.get(baseDir.getAbsolutePath() + File.separator + srcFile.getName());
 		Files.copy(srcFile.toPath(), copied, StandardCopyOption.REPLACE_EXISTING);*/
 
 		//deletar pasta
-		FileUtils.deleteDirectory(baseDir);
-		
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				System.out.println("Shutdown Hook is running !");
-			}
-		});
-		
-		System.out.println("Application Terminating ..."); 
+//		FileUtils.deleteDirectory(baseDir);
+//		
+//		Runtime.getRuntime().addShutdownHook(new Thread() {
+//			@Override
+//			public void run() {
+//				System.out.println("Shutdown Hook is running !");
+//			}
+//		});
+//		
+//		System.out.println("Application Terminating ..."); 
 
+		
+		File file = new File("anelar-dir.wsq");
+		byte[] img = Files.readAllBytes(file.toPath());
+		
+		String contentType = new Tika().detect(img);
+		
+		System.out.println(contentType);
+		
 	}
 
 }
