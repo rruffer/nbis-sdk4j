07819d9894d3f1f87a5915c571c48c2da0839e66
rruffer <alucard@22>
2019-01-31 20:40:05 -0200

    ajustando constante para funcionar no linux e no windows

--- src/main/java/br/com/nbis/util/UtilConstants.java
+++ src/main/java/br/com/nbis/util/UtilConstants.java
@@ -1,7 +1,9 @@
 package br.com.nbis.util;
 
+import java.io.File;
+
 public class UtilConstants {
 	
-	public static final String TEMP_DIR_NBIS = System.getProperty("java.io.tmpdir") + "nbis";
+	public static final String TEMP_DIR_NBIS = System.getProperty("java.io.tmpdir") + File.separator + "nbis";
 
 }
