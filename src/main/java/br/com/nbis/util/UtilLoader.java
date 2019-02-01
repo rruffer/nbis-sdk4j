2913c867999521d6d775d6df97f94e3b852dfd86
rruffer <alucard@22>
2019-02-01 21:08:41 -0200

    mudando método que gera um arquivo de um imputfile

--- src/main/java/br/com/nbis/util/UtilLoader.java
+++ src/main/java/br/com/nbis/util/UtilLoader.java
@@ -15,117 +15,133 @@ import org.apache.tika.Tika;
 import br.com.nbis.exeption.NbisException;
 
 public class UtilLoader {
-	
+
 	private static final Logger log = LogManager.getLogger(UtilLoader.class);
 
 	private UtilLoader() {
 		// Classe de serviço
 	}
-	
+
 	/**
 	 * Carrrega os arquivos
+	 * 
 	 * @param fileName
 	 * @return
 	 */
 	public static File getFile(String fileName) {
-		
-		InputStream stream = UtilLoader.class.getResourceAsStream(fileName);
-		File file = null;
-		
-		try {
-			file = File.createTempFile("tmp", null, null);
+
+		File file = new File(UtilConstants.TEMP_DIR_NBIS + File.separator + "cwsq.exe");
+
+		try (InputStream stream = UtilLoader.class.getResourceAsStream(fileName)){
+			//file = File.createTempFile("tmp", null, null);
 			log.debug(file.getAbsolutePath());
 			FileUtils.copyInputStreamToFile(stream, file);
 		} catch (Exception e) {
 			log.error("Erro ao criar arquivo temporário: ", e);
 		}
-		
-		
+
 		return file;
-		
+
 	}
 
-	
+	/**
+	 * Carrrega os arquivos
+	 * 
+	 * @param fileName
+	 * @return
+	 *//*
+		 * public static File getFile(String fileName) {
+		 * 
+		 * InputStream stream = UtilLoader.class.getResourceAsStream(fileName); File
+		 * file = null;
+		 * 
+		 * try { file = File.createTempFile("tmp", null, null);
+		 * log.debug(file.getAbsolutePath()); FileUtils.copyInputStreamToFile(stream,
+		 * file); } catch (Exception e) {
+		 * log.error("Erro ao criar arquivo temporário: ", e); }
+		 * 
+		 * 
+		 * return file;
+		 * 
+		 * }
+		 */
+
 	public static File copyFileForTempDir(String fileName) throws IOException {
-		
+
 		return copyFileForTempDir(new File(fileName));
-		
+
 	}
-	
+
 	public static File copyFileForTempDir(File srcFile) throws IOException {
-		
+
 		File baseDir = new File(UtilConstants.TEMP_DIR_NBIS);
-		
-		if(baseDir.exists()) {
+
+		if (baseDir.exists()) {
 			FileUtils.deleteDirectory(baseDir);
 		}
-		
+
 		baseDir.mkdir();
-		
+
 		log.debug(baseDir.getAbsolutePath());
-		
+
 		// forma 1
 		FileUtils.copyFileToDirectory(srcFile, baseDir);
-		
+
 		return new File(baseDir.getAbsolutePath() + File.separator + srcFile.getName());
-		
+
 	}
-	
+
 	public static File createFileInTempDir(byte[] img) throws IOException, NbisException {
-		
+
 		ContentType contentType = new ContentType(img);
-		
-		if(!contentType.getType().equals("image")) {
+
+		if (!contentType.getType().equals("image")) {
 			throw new NbisException("Arquivo não é uma imagem!");
 		}
-		
+
 		File baseDir = new File(UtilConstants.TEMP_DIR_NBIS);
-		
-		if(baseDir.exists()) {
+
+		if (baseDir.exists()) {
 			FileUtils.deleteDirectory(baseDir);
 		}
-		
+
 		baseDir.mkdir();
-		
+
 		Path path = Paths.get(baseDir.getAbsolutePath() + File.separator + "outputWSQ." + contentType.getExtension());
-		
+
 		try {
 			Files.write(path, img);
 		} catch (IOException e) {
 			log.error("Erro ao converter imagem: ", e);
 		}
-		
+
 		return path.toFile();
-		
+
 	}
-	
-	
-	
-	/*public File getFile(String fileName) {
-		
-		InputStream stream = getClass().getResourceAsStream(fileName);
-		File file = null;
-		try {
-//			file = File.createTempFile("tmp", ".exe", new File("D:/")); 
-			file = File.createTempFile("tmp", null, null);
-			log.debug(file.getAbsolutePath());
-			FileUtils.copyInputStreamToFile(stream, file);
-		} catch (IOException e) {
-			log.error("Erro ao criar arquivo temporário: ", e);
-		}
-		
-		
-		return file;
-		
-	}*/
-	
+
+	/*
+	 * public File getFile(String fileName) {
+	 * 
+	 * InputStream stream = getClass().getResourceAsStream(fileName); File file =
+	 * null; try { // file = File.createTempFile("tmp", ".exe", new File("D:/"));
+	 * file = File.createTempFile("tmp", null, null);
+	 * log.debug(file.getAbsolutePath()); FileUtils.copyInputStreamToFile(stream,
+	 * file); } catch (IOException e) {
+	 * log.error("Erro ao criar arquivo temporário: ", e); }
+	 * 
+	 * 
+	 * return file;
+	 * 
+	 * }
+	 */
+
 }
 
 class ContentType {
-	
+
 	private String type;
 	private String extension;
-	
+
 	public ContentType(byte[] img) {
 		String contentType = new Tika().detect(img);
 		String[] array = contentType.split("/");
@@ -148,5 +164,5 @@ class ContentType {
 	public void setExtension(String extension) {
 		this.extension = extension;
 	}
-	
+
 }
