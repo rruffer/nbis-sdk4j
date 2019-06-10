9ccc20362b7959a3935a1163f30b673d89022bd7
rruffer <rodolforuffer@yahoo.com.br>
2019-06-10 09:17:14 -0300

    .

--- src/test/java/br/com/nbis/SuiteTest.java
+++ src/test/java/br/com/nbis/SuiteTest.java
@@ -2,11 +2,12 @@ package br.com.nbis;
 
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-//@SelectClasses({Bozorth3Test.class})
 @SelectPackages("br.com.nbis")
+@SuiteDisplayName("Testes SKD Nbis 5.0.0")
 public class SuiteTest {
 
 }
